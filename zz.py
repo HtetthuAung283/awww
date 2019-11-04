f7c1b8255ff4b5cb59a75f846e4079ecf6885cd3
HtetthuAung283 <htetthuaung283@gmail.com>
2019-11-05 00:04:32 +0630

    zz

---
+++ zz.py
@@ -0,0 +1,12 @@
+x = int(input ("Please enter an integer :"))
+
+if x < 5:
+        print ('x is smaller than 5')
+elif x > 10:
+         print('x is bigger than 10')
+
+elif x == 7:
+        print('x is equal to 7')
+else :
+
+    	print('x is undefinied')
